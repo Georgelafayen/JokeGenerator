23f220148b55af834a6b29fe484c36be019e3fe4
Georgelafayen <giorgio.faini@gmail.com>
2023-10-18 20:26:24 +0200

    New version updated

--- index.js
+++ index.js
@@ -8,43 +8,48 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-
 app.use(express.static(__dirname + "/public/styles/"));
 app.use(express.static(__dirname + "/public/images/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const category = ["Programming", "Misc", "Dark", "Pun"];
+const type = ["single", "twopart"];
+
+let rCat = Math.floor(Math.random() * 4);
+let rTy = Math.floor(Math.random() * 2);
+
+let cat = category[rCat];
+let ty = type[rTy];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {joke: "", setup: "", delivery: ""});
-    console.log("Ciao beccati sta pagina!");
+  res.render("index.ejs", { joke: "", setup: "", delivery: "" });
+  console.log("Ciao beccati sta pagina!");
 });
 
-app.post("/", async (req, res) => {
-  const category = req.body.category;
-  const type = req.body.type;
+app.get("/joke", async (req, res) => {
+  try {
+    let rCat = Math.floor(Math.random() * 4);
+    let rTy = Math.floor(Math.random() * 2);
+    let cat = category[rCat];
+    let ty = type[rTy];
 
-    try {
-      const result = await axios.get(`https://v2.jokeapi.dev/joke/${category}?type=${type}`);
-      res.render("index.ejs", { 
-        joke: result.data.joke, 
-        setup : result.data.setup, 
-        delivery: result.data.delivery,
-      });
-      console.log("Ciao beccati sti risultati!");
+    const result = await axios.get(
+      `https://v2.jokeapi.dev/joke/${cat}?type=${ty}`
+    );
+    res.render("index.ejs", {
+      joke: result.data.joke,
+      setup: result.data.setup,
+      delivery: result.data.delivery,
+    });
 
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-  });
-
+    console.log("Ciao beccati sti risultati!");
+    console.log(cat, ty);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-
-
-
-
-  
+  console.log(`Server is running on port ${port}`);
+});
